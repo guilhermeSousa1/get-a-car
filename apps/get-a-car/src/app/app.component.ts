0203b5bb46c6e5f15a79bae77ee64dbe41b86d86
Guilherme Sousa <56938763+guilhermeSousa1@users.noreply.github.com>
2021-03-05 23:29:50 +0000

    initial commit

---
+++ apps/get-a-car/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'guilherme-sousa1-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'get-a-car';
+}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/get-a-car'],
+};
