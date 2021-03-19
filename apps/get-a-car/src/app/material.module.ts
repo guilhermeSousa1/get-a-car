89fd65fcf680c6deaae33e6063ccc1f586306234
Guilherme Sousa <56938763+guilhermeSousa1@users.noreply.github.com>
2021-03-19 23:43:10 +0000

    Added the delivery and collection time form fields

--- apps/get-a-car/src/app/material.module.ts
+++ apps/get-a-car/src/app/material.module.ts
@@ -9,6 +9,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 const MODULES = [
   MatToolbarModule,
@@ -24,7 +25,8 @@ const MODULES = [
   MatFormFieldModule,
   MatInputModule,
   MatDatepickerModule,
-  MatNativeDateModule
+  MatNativeDateModule,
+  MatSelectModule
 ];
 
 /** Custom options to configure form field options */
