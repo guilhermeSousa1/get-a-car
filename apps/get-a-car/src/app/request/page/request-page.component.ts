2910816a3bbcc8c226381543eb7d6c4268174a93
Guilherme Sousa <56938763+guilhermeSousa1@users.noreply.github.com>
2021-03-21 18:03:01 +0000

    The "request-a-car" form has validations and error handlers.

--- apps/get-a-car/src/app/request/page/request-page.component.ts
+++ apps/get-a-car/src/app/request/page/request-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { finalize, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { EditCarPreferencesDialogComponent } from '../dialogs/edit-car-preferences/edit-car-preferences.dialog.component';
 
@@ -13,18 +16,42 @@ import { EditCarPreferencesDialogComponent } from '../dialogs/edit-car-preferenc
 })
 export class RequestPageComponent implements OnInit {
 
+  /** Form group to be used by the form */
+  public form: FormGroup;
+  /** Observable for the small screen size. */
+  public isSmallScreen$: Observable<boolean>;
+  /** List of possible time values, hourly separated, of a day */
+  public timesList = [
+    ...Array.from({ length: 12 }).map((_, hr) => ({ display: `${ hr === 0 ? 12 : hr }:00 AM`, value: hr })),
+    ...Array.from({ length: 12 }).map((_, hr) => ({ display: `${ hr === 0 ? 12 : hr }:00 PM`, value: hr + 12 }))
+  ]
+
   /**
    * Class constructor.
    *
    * @public
    *
-   * @param dialog  Injection of the Dialog service
+   * @param dialog              Injection of the Dialog service
+   * @param formBuilder         Injection of the FormBuilder service
+   * @param breakPointObserver  Injection of the breakpoint observer utility
    */
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog,
+              private formBuilder: FormBuilder,
+              private breakPointObserver: BreakpointObserver) {
   }
 
-  ngOnInit(): void {
-    // Empty
+  /**
+   * Lifecycle hook that is executed after the component is initialized.
+   *
+   * @public
+   */
+  public ngOnInit(): void {
+    this.isSmallScreen$ = this.breakPointObserver.observe('(max-width: 639px)')
+      .pipe(
+        map(((result) => result.matches))
+      );
+
+    this.initializeForm();
   }
 
   /**
@@ -34,7 +61,7 @@ export class RequestPageComponent implements OnInit {
    */
   public editCarPreferences(): void {
     const config: MatDialogConfig = {
-      width:     '552px',
+      width:     '550px',
       autoFocus: false
     };
 
@@ -43,10 +70,44 @@ export class RequestPageComponent implements OnInit {
     dialogRef.afterClosed()
       .pipe(
         untilDestroyed(this),
-        tap(() => console.log('dialog closed')),
-        finalize(() => console.log('subscription finalized'))
+        tap(() => console.log('dialog closed'))
       )
       .subscribe();
   }
+
+  /**
+   * Initializes the form
+   *
+   * @private
+   */
+  private initializeForm(): void {
+    this.form = this.formBuilder.group({
+      address:        [null, Validators.required],
+      startDate:      [null, Validators.required],
+      endDate:        [null, Validators.required],
+      deliveryTime:   [null, Validators.required],
+      collectionTime: [null, Validators.required]
+    }, { validators: this.invalidSameDayReservation });
+  }
+
+  /**
+   * Validates that the delivery and collection times are possible for same day car reservation.
+   *
+   * @param control  The form control
+   * @returns        {ValidationErrors | null}
+   */
+  private invalidSameDayReservation: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    if (control.value?.['startDate'] != null
+      && control.value?.['endDate'] != null
+      && control.value?.['deliveryTime'] != null
+      && control.value?.['collectionTime'] != null
+      && +control.value?.['startDate'] === +control.value?.['endDate']
+      && control.value?.['deliveryTime'] > control.value?.['collectionTime']) {
+      return { invalidSameDayReservation: true };
+    } else {
+      return null;
+    }
+  };
+
 
 }
