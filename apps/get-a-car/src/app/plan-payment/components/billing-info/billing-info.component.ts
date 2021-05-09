367c0b1e14ac982f6f5f800c35b5f12406717645
Guilherme Sousa <56938763+guilhermeSousa1@users.noreply.github.com>
2021-05-09 11:44:34 +0100

    Implementation of the billing-info component.

---
+++ apps/get-a-car/src/app/plan-payment/components/billing-info/billing-info.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+/* eslint-disable no-multi-spaces */
+@Component({
+  selector:    'billing-info',
+  templateUrl: './billing-info.component.html',
+  styleUrls:   ['./billing-info.component.scss']
+})
+export class BillingInfoComponent {
+
+  /**
+   * Class constructor.
+   *
+   * @public
+   */
+  public constructor() {
+  }
+
+}
--- apps/get-a-car/src/app/plan-payment/plan-payment.module.ts
+++ apps/get-a-car/src/app/plan-payment/plan-payment.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@guilhermeSousa1/material.module';
 import { PlanPaymentRoutingModule } from '@guilhermeSousa1/plan-payment/plan-payment-routing.module';
 import { PlanPaymentPageComponent } from './page/plan-payment-page.component';
+import { BillingInfoComponent } from './components/billing-info/billing-info.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { PlanPaymentPageComponent } from './page/plan-payment-page.component';
     MaterialModule
   ],
   declarations: [
-    PlanPaymentPageComponent
+    PlanPaymentPageComponent,
+    BillingInfoComponent
   ]
 })
 export class PlanPaymentModule {
