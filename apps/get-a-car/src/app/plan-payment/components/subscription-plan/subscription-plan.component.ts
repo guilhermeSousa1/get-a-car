0f540c0f9b3f5bf2c77d9674d75541ce7f86e28a
Guilherme Sousa <56938763+guilhermeSousa1@users.noreply.github.com>
2021-05-11 14:00:54 +0100

    Creation of the subscription-plan component

---
+++ apps/get-a-car/src/app/plan-payment/components/subscription-plan/subscription-plan.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+/* eslint-disable no-multi-spaces */
+@Component({
+  selector:    'subscription-plan',
+  templateUrl: './subscription-plan.component.html',
+  styleUrls:   ['./subscription-plan.component.scss']
+})
+export class SubscriptionPlanComponent {
+
+  /**
+   * Class constructor.
+   *
+   * @public
+   */
+  constructor() {
+  }
+
+}
--- apps/get-a-car/src/app/plan-payment/plan-payment.module.ts
+++ apps/get-a-car/src/app/plan-payment/plan-payment.module.ts
@@ -5,9 +5,10 @@ import { CreditCardDirectivesModule } from 'angular-cc-library';
 import { MaterialModule } from '@guilhermeSousa1/material.module';
 import { PlanPaymentRoutingModule } from '@guilhermeSousa1/plan-payment/plan-payment-routing.module';
 import { EditBillingInfoDialogComponent } from '@guilhermeSousa1/plan-payment/dialogs/edit-billing-info/edit-billing-info.dialog.component';
+import { PipesModule } from '@guilhermeSousa1/shared/pipes/pipes.module';
 import { PlanPaymentPageComponent } from './page/plan-payment-page.component';
 import { BillingInfoComponent } from './components/billing-info/billing-info.component';
-import { PipesModule } from '@guilhermeSousa1/shared/pipes/pipes.module';
+import { SubscriptionPlanComponent } from './components/subscription-plan/subscription-plan.component';
 
 @NgModule({
   imports: [
@@ -21,7 +22,8 @@ import { PipesModule } from '@guilhermeSousa1/shared/pipes/pipes.module';
   declarations: [
     PlanPaymentPageComponent,
     BillingInfoComponent,
-    EditBillingInfoDialogComponent
+    EditBillingInfoDialogComponent,
+    SubscriptionPlanComponent
   ]
 })
 export class PlanPaymentModule {
